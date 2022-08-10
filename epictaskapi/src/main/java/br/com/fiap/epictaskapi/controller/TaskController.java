636b0b9a5e32ffa1c02d4a0d56b1d08d62ed2baf
Joao <joaocarloslima@me.com>
2022-08-10 11:56:51 -0300

    :wastebasket: Spring Data

--- epictaskapi/src/main/java/br/com/fiap/epictaskapi/controller/TaskController.java
+++ epictaskapi/src/main/java/br/com/fiap/epictaskapi/controller/TaskController.java
@@ -2,22 +2,48 @@ package br.com.fiap.epictaskapi.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.epictaskapi.model.Task;
 import br.com.fiap.epictaskapi.service.TaskService;
 
 @RestController
+@RequestMapping("/api/task")
 public class TaskController {
 
     @Autowired
     private TaskService service;
     
-    @GetMapping("/api/task")
+    @GetMapping()
     public List<Task> index(){
         return service.listAll();
     }
+
+    @PostMapping()
+    public ResponseEntity<Task> create(@RequestBody @Valid Task task){
+        service.save(task);
+        
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(task);
+    }
+
+    @GetMapping("{id}")
+    public Task show(@PathVariable Long id){
+        Task task = service.getById(id);
+        System.out.println(task);
+        return task;
+    }
+
 
 }
--- epictaskapi/src/main/java/br/com/fiap/epictaskapi/service/TaskService.java
+++ epictaskapi/src/main/java/br/com/fiap/epictaskapi/service/TaskService.java
@@ -1,21 +1,29 @@
 package br.com.fiap.epictaskapi.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.fiap.epictaskapi.model.Task;
+import br.com.fiap.epictaskapi.repository.TaskRepository;
 
 @Service
 public class TaskService {
     
+    @Autowired
+    TaskRepository repository;
+    
     public List<Task> listAll(){
-        return List.of( 
-            new Task("Modalagem do BD", "Modelagem das tabelas do banco de dados", 100, 0),
-            new Task("Prototipação", "Prototipar a interface gráfica", 100, 0)
-        );
+        return repository.findAll();
+    }
 
+    public void save(Task task) {
+        repository.save(task);
+    }
+
+    public Task getById(Long id) {
+        return repository.getReferenceById(id);
     }
 
 }
