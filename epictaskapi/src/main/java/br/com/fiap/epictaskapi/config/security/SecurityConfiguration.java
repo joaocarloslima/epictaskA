095098eaaff0ef6921bc3dd86017a5f4166f119d
Joao <joaocarloslima@me.com>
2022-09-21 12:05:16 -0300

    :key: JWT

--- epictaskapi/src/main/java/br/com/fiap/epictaskapi/config/security/SecurityConfiguration.java
+++ epictaskapi/src/main/java/br/com/fiap/epictaskapi/config/security/SecurityConfiguration.java
@@ -3,14 +3,13 @@ package br.com.fiap.epictaskapi.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfiguration {
@@ -18,14 +17,17 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http    
-            .httpBasic()
-            .and()
+            // .httpBasic()
+            // .and()
             .authorizeHttpRequests()
                 .antMatchers(HttpMethod.GET, "/api/task/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/task/").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/api/task").authenticated()
+                .antMatchers(HttpMethod.POST, "/api/auth").permitAll()
                 .anyRequest().denyAll()
             .and() 
                 .csrf().disable()
+                .addFilterBefore(new AuthorizationFilter(), 
+                                UsernamePasswordAuthenticationFilter.class)
         ;        
         
         return http.build();
@@ -45,6 +47,11 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
     }
     
 }
--- epictaskapi/src/main/java/br/com/fiap/epictaskapi/model/User.java
+++ epictaskapi/src/main/java/br/com/fiap/epictaskapi/model/User.java
@@ -1,20 +1,36 @@
 package br.com.fiap.epictaskapi.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.ListIterator;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import net.bytebuddy.agent.builder.AgentBuilder.PoolStrategy.Eager;
+
 
 @Entity
 @Table(name = "TB_USER")
-public class User {
+public class User implements UserDetails{
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
 
     public User() {
     }
@@ -47,6 +63,36 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
