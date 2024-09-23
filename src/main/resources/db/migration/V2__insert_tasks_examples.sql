f807d307daced3dd5d486a2f895755706c0484b5
joaocarloslima <joaocarloslima@me.com>
2024-09-23 09:54:43 -0300

    consulta de tarefas

---
+++ src/main/resources/db/migration/V2__insert_tasks_examples.sql
@@ -0,0 +1,4 @@
+INSERT INTO task (id, title, description, status, score) VALUES
+('4ede5b29-f3b2-42ee-a100-a26f4e0bcf60', 'Implement Authentication', 'Implement user authentication using Spring Security', 50, 10),
+('36e0429a-8c47-4194-9419-a5d2b605c140', 'Database Migration', 'Create and apply database migrations using Flyway', 75, 20),
+('8727568e-eb67-4719-bfa5-9d7f08fae4fb', 'API Documentation', 'Document the REST API endpoints using Swagger', 25, 30);
